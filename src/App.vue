7f76da2d405b487a97cde4ab93a80c71daa7dea0
christian <delmsnoob@gmail.com>
2021-12-06 19:17:45 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <Header title="Website todo"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="nav-bar">
+        <i class="fas fa-bars"></i>
+        <h2> {{ title }} </h2>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: center;
+}
+
+h2 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #fff;
+}
+
+.nav-bar {
+  background: #79d9a1;
+  height: 60px;
+  margin-bottom: 25px;
+  /* -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57); */
+  /* -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57); */
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
+}
+
+.fas {
+  color: #fff;
+  font-size: 20px;
+  margin: 20px;
+}
+</style>
